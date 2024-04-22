e032082b6cb0a6d3c72c89f56849259dde41eb3b
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-04-22 17:38:51 +0200

    commit limpio del ejercicio en casa

---
+++ Ejercicio-Casa-Ramas/src/main/java/com/salesianostriana/dam/ejerciciocasaramas/EjercicioCasaRamasApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejerciciocasaramas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioCasaRamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioCasaRamasApplication.class, args);
+	}
+
+}
---
+++ Ejercicio-Casa-Ramas/src/test/java/com/salesianostriana/dam/ejerciciocasaramas/EjercicioCasaRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejerciciocasaramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioCasaRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
